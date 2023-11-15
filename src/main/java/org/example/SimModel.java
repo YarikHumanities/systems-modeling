98c96f47f1b61c6e8a8490741c75b8c141396f34
Yarik <yaroslav.matsapura@gmail.com>
2023-11-15 17:00:49 +0200

    add row setup for modeling bank system

--- src/main/java/org/example/SimModel.java
+++ src/main/java/org/example/SimModel.java
@@ -5,34 +5,27 @@ import java.util.ArrayList;
 public class SimModel {
     public static void main(String[] args) throws Exception {
 
-        Create c = new Create("Create-1", 2.0, true);
-        Process p1 = new Process("Process-1", 1.0, 10, 3, true);
-        Process p2 = new Process("Process-2", 1.0, 10, 3, true);
-        Process p3 = new Process("Process-3", 1.0, 90, 2, true);
+        Create c = new Create("Create-1", 0.5, false);
+        Process p1 = new Process("Process-1", 0.3, 10, 10, false);
+        Process p2 = new Process("Process-2", 0.3, 10, 1, false);
 
        c.setNextElementsList(new ArrayList<>(){{
            add(p1);
-       }});
-       p1.setNextElementsList(new ArrayList<>(){{
            add(p2);
-           add(p3);
        }});
 
 
-        p1.setMaxqueue(5);
-        p2.setMaxqueue(5);
-        p3.setMaxqueue(5);
+        p1.setMaxqueue(3);
+        p2.setMaxqueue(3);
 
         c.setDistribution("exp");
         p1.setDistribution("exp");
         p2.setDistribution("exp");
-        p3.setDistribution("exp");
 
         ArrayList<Element> list = new ArrayList<>();
         list.add(c);
         list.add(p1);
         list.add(p2);
-        list.add(p3);
 
         Model model = new Model(list);
         model.simulate(1000.0);
