ed36a01911c6d344725c426b36f2e4d1012bd689
Yarik <yaroslav.matsapura@gmail.com>
2023-11-20 23:25:42 +0200

    add loop for multiple simulations

--- src/main/java/org/example/SimModel.java
+++ src/main/java/org/example/SimModel.java
@@ -1,13 +1,29 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SimModel {
-    public static int N = 2;
     public static void main(String[] args) throws Exception {
-        simpleModelSet();
+
+        List<Integer> numberList = List.of(10, 100);
+        ArrayList<Long> times = new ArrayList<>();
+        for (Integer N : numberList) {
+            long startTime = System.nanoTime();
+            simpleModelSet(N);
+            long endTime = System.nanoTime();
+            long elapsedTime = endTime - startTime;
+            times.add(elapsedTime);
+        }
+
+        System.out.println();
+        for(int i = 0; i<numberList.size(); i++){
+            System.out.println("Time for " + numberList.get(i) + " processes: " + times.get(i));
+        }
+
+        simpleModelSet(100);
     }
-    public static void simpleModelSet() throws Exception {
+    public static void simpleModelSet(int N) throws Exception {
         Create c = new Create("Create-1", 2.0, true);
         c.setDistribution("exp");
         ArrayList<Element> list = new ArrayList<>();
@@ -16,7 +32,6 @@ public class SimModel {
         for(int i = 0; i<N; i++){
             String processName = "Process-" + (i + 1);
             Process p1 = new Process(processName, 1.0, 10, 3, true);
-            p1.setMaxqueue(5);
             p1.setDistribution("exp");
             list.add(p1);
             if(i==0){
